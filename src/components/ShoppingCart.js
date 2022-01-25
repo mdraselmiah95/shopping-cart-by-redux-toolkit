022d43480567ef5beb2365b45d22a84cd734a215
rasel <russelroy15@gmail.com>
2022-01-25 20:04:23 +0600

    WIP on Shopping cart with Redux toolkit and Tailwind CSS

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -14,26 +14,26 @@ const ShoppingCart = () => {
       </h2>
       <div className="bg-BG-Gray p-12">
         <div className="container bg-white m-auto rounded-md">
-          <div className="flex flex-row px-4 py-3">
-            <div className="flex item-center justify-start items-center w-2/3">
+          <div className="md:flex md:flex-row px-4 py-3">
+            <div className="flex item-center justify-start items-center xl:w-2/3 md:w-1/2 ">
               <img src={img1} alt="productOne" className="h-36" />
               <h5 className="text-xl font-bold px-7">iPhone 11 128GB Black</h5>
             </div>
-            <div className="flex item-center justify-start items-center w-1/3">
+            <div className="flex item-center justify-start items-center xl:w-1/3 md:w-1/2">
               <button className="hover:bg-zinc-300 rounded-md hover:text-emerald-700 p-2">
                 {PlusSquare}
               </button>
               <input
                 type="number"
                 min="0"
-                className="text-center border-none text-xl w-16 font-bold py-2 rounded-lg mx-3"
+                className="text-center border-none text-xl w-16 font-bold py-2 rounded-lg md:mx-3 mx-1"
                 value="1"
               ></input>
               <button className="hover:bg-zinc-300 rounded-md hover:text-emerald-700 p-2">
                 {PlusMinus}
               </button>
-              <h5 className="text-lg font-bold px-10">$1219</h5>
-              <img src={remove} alt="remove" />
+              <h5 className="text-lg font-bold md:px-10 px-4">$1219</h5>
+              <img src={remove} alt="remove" className="cursor-pointer" />
             </div>
           </div>
         </div>
