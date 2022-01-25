09dea1bd4abb97f0e47a80d6b04a5bc97b97027e
rasel <russelroy15@gmail.com>
2022-01-25 16:49:18 +0600

    WIP on Shopping cart with Reduc toolkit

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -3,7 +3,7 @@ import React from "react";
 const ShoppingCart = () => {
   return (
     <div>
-      <h2>This is Cart.</h2>
+      <h2 class="text-2xl text-fuchsia-500">This is Cart.</h2>
     </div>
   );
 };
