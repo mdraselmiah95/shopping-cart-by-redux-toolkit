f8f5a352e0c3971450020855e4cb44fdcb547d63
rasel <russelroy15@gmail.com>
2022-01-25 19:32:48 +0600

    WIP on Shopping cart with Redux toolkit and Tailwind CSS

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -14,20 +14,24 @@ const ShoppingCart = () => {
       </h2>
       <div className="bg-BG-Gray p-12">
         <div className="container bg-white m-auto rounded-md">
-          <div className="flex flex-row">
+          <div className="flex flex-row px-4 py-3">
             <div className="flex item-center justify-start items-center w-2/3">
               <img src={img1} alt="productOne" className="h-36" />
               <h5 className="text-xl font-bold px-7">iPhone 11 128GB Black</h5>
             </div>
             <div className="flex item-center justify-start items-center w-1/3">
-              <button className="">{PlusSquare}</button>
+              <button className="hover:bg-zinc-300 rounded-md hover:text-emerald-700 p-2">
+                {PlusSquare}
+              </button>
               <input
                 type="number"
                 min="0"
-                className="text-center border-none text-xl w-16 font-bold py-2 rounded-lg mx-6"
+                className="text-center border-none text-xl w-16 font-bold py-2 rounded-lg mx-3"
                 value="1"
               ></input>
-              <button className="">{PlusMinus}</button>
+              <button className="hover:bg-zinc-300 rounded-md hover:text-emerald-700 p-2">
+                {PlusMinus}
+              </button>
               <h5 className="text-lg font-bold px-10">$1219</h5>
               <img src={remove} alt="remove" />
             </div>
